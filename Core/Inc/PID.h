6e10436e4cdb06a4faf9a1f7f652283f86e54eb2
Piotr Woliński <pwolinski03@gmail.com>
2025-02-10 22:16:10 +0100

    Separated pid and filter init

--- Core/Inc/PID.h
+++ Core/Inc/PID.h
@@ -30,25 +30,33 @@ struct PID_State
 	float _previousInput;
 
 	// Filter State
-	float* _previousInputRaw;
+	float* _previousInputRawBuff;
 	uint16_t _previousInputBufferPointer;
 };
 
 /*
  * @brief Initializes PID internal structure
- * @param[out] *regulator - pointer to pid struct to initialize
- * @param[in]   K - proportional gain
- * @param[in]   I - integral gain
- * @param[in]   D - differential gain
- * @param[in]   dTermFiterOrder - order of differential filtering (0 for no filtering)
- * @param[in]  *firCoefs - pointer to an array of FIR coefficients,
- * 			    array size = dTermFiterOrder + 1,
- * 			    can be null if filter is off
+ * @param[out] *regulator pointer to pid struct to initialize
+ * @param[in]   K proportional gain
+ * @param[in]   I integral gain
+ * @param[in]   D differential gain
  * @return 	    bool
  * 			    - true initialization complete
- * 			    - initialization failed
+ * 			    - false initialization failed
  */
-bool PID_Innit(struct PID_State* regulator, float K, float I, float D, uint16_t dTermFiterOrder, const float* firCoefs);
+bool PID_Init(struct PID_State* regulator, float K, float I, float D);
+
+/*
+ * @brief Initializes PID internal structure
+ * @param[out] *regulator pointer to pid struct to initialize
+ * @param[in]   dTermFiterOrder order of differential filtering (0 for no filtering)
+ * @param[in]  *firCoefs pointer to an array of FIR coefficients,
+ * 			    array size = dTermFiterOrder + 1
+ * @return 	    bool
+ * 			    - true initialization complete
+ * 			    - false initialization failed
+ */
+bool PID_DFilterInit(struct PID_State* regulator, uint16_t dTermFiterOrder, const float* firCoefs);
 
 /*
  * @brief Calculates PID response based on given input and updates internal regulator state
--- Core/Src/PID.c
+++ Core/Src/PID.c
@@ -9,26 +9,32 @@
 #include <stdlib.h>
 
 
-float _clamp(float num, float min, float max)
+static float _clamp(float num, float max, float min)
 {
 	const float t = num < min ? min : num;
 	return t > max ? max : t;
 }
 
-bool PID_Innit(struct PID_State* regulator, float K, float I, float D, uint16_t dTermFiterOrder, const float* firCoefs)
+bool PID_Init(struct PID_State* regulator, float K, float I, float D)
 {
 	regulator->K_Gain = K;
 	regulator->I_Gain = I;
 	regulator->D_Gain = D;
 
-	regulator->_integralState              = 0;
+	regulator->_integralState = 0;
+	regulator->_previousInput = 0;
+
 	regulator->_previousInputFiltered      = 0;
-	regulator->_previousInput              = 0;
 	regulator->_previousInputBufferPointer = 0;
 
 	regulator->MaxSaturation = __builtin_inff();
 	regulator->MinSaturation = -__builtin_inff();
 
+	return true;
+}
+
+bool PID_DFilterInit(struct PID_State* regulator, uint16_t dTermFiterOrder, const float* firCoefs)
+{
 	if(dTermFiterOrder == 0)
 	{
 		regulator->FIR_Coefficients = NULL;
@@ -39,15 +45,17 @@ bool PID_Innit(struct PID_State* regulator, float K, float I, float D, uint16_t 
 		return false;
 	}
 
-	regulator->_previousInputRaw = (float*)malloc(dTermFiterOrder * sizeof(float));
-	if(regulator->_previousInputRaw == NULL)
+	regulator->_previousInputBufferPointer = 0;
+
+	regulator->_previousInputRawBuff = (float*)malloc(dTermFiterOrder * sizeof(float));
+	if(regulator->_previousInputRawBuff == NULL)
 	{
 		return false;
 	}
 
 	for(uint16_t i = 0; i < dTermFiterOrder; i++)
 	{
-		regulator->_previousInputRaw[i] = 0;
+		regulator->_previousInputRawBuff[i] = 0;
 	}
 
 	return true;
@@ -57,15 +65,17 @@ float PID_Update(struct PID_State* regulator, float input, float dt)
 {
 	float output = 0;
 
+	// Proportional part
 	output += input * regulator->K_Gain;
 
-	regulator->_integralState += (input + regulator->_previousInput) * 0.5 * dt;
+	// Integral part
+	regulator->_integralState += (input + regulator->_previousInput) * 0.5 * regulator->I_Gain * dt;
 	regulator->_integralState  = _clamp(regulator->_integralState, regulator->MaxSaturation, regulator->MinSaturation);
+	regulator->_previousInput  = input;
 
-	regulator->_previousInput = input;
+	output += regulator->_integralState;
 
-	output += regulator->_integralState * regulator->I_Gain;
-
+	// Derivative part
 	float filteredInput = 0;
 	if(regulator->D_FilterOrder > 0)
 	{
@@ -73,18 +83,17 @@ float PID_Update(struct PID_State* regulator, float input, float dt)
 		{
 			uint16_t n = (i + regulator->_previousInputBufferPointer) % regulator->D_FilterOrder;
 
-			filteredInput += regulator->_previousInputRaw[n] * regulator->FIR_Coefficients[i];
+			filteredInput += regulator->_previousInputRawBuff[n] * regulator->FIR_Coefficients[i];
 		}
 		filteredInput += input * regulator->FIR_Coefficients[regulator->D_FilterOrder];
 
 		regulator->_previousInputBufferPointer = ++(regulator->_previousInputBufferPointer) % regulator->D_FilterOrder;
-		regulator->_previousInputRaw[regulator->_previousInputBufferPointer] = input;
+		regulator->_previousInputRawBuff[regulator->_previousInputBufferPointer] = input;
 	}
 	else
 	{
 		filteredInput = input;
 	}
-
 	output += (filteredInput - regulator->_previousInputFiltered) / dt;
 
 	regulator->_previousInputFiltered = filteredInput;
@@ -95,8 +104,8 @@ float PID_Update(struct PID_State* regulator, float input, float dt)
 
 void PID_Destroy(struct PID_State* regulator)
 {
-	if(regulator->_previousInputRaw != NULL)
+	if(regulator->_previousInputRawBuff != NULL)
 	{
-		free(regulator->_previousInputRaw);
+		free(regulator->_previousInputRawBuff);
 	}
 }
